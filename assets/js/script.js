3b5a2968df1760dce7273af5877d43c5243738a7
Matthew Pryor <matthewapryor@gmail.com>
2022-04-05 14:37:29 -0400

    understanding luxon more

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,26 @@
 let DateTime = luxon.DateTime;
+let dtl = DateTime.local();
+let today = DateTime.local().toLocaleString(DateTime.DATETIME_MED);
 
 let dateContainer = $('#date-container');
-let today = DateTime.local().toLocaleString(DateTime.DATETIME_MED);
 let currentDateEl = document.createElement('p');
 $(currentDateEl).attr('current-date')
 $(currentDateEl).addClass('text-center text-xl font-black')
 currentDateEl.innerHTML = today;
 dateContainer.append(currentDateEl);
-// currentDateText = today;
+
+setStart = () => {
+  dtl.set({ hour: 9 }).hour;
+};
+console.log(dayStart);
+
+let hourContainer = $('#hour-container');
+let hourEl = document.createElement('p');
+
+for (var i = 0; i < 9; i++) {
+  $(hourEl).attr('hour');
+  $(hourEl).addClass('text-right flex-end items-center bg-yellow-400');
+  hourEl.innerHTML = (i + 1) + ':00';
+}
+
+
