ee632fa2a9d2b2c453c70cb00b09b0dd44800531
Matthew Pryor <matthewapryor@gmail.com>
2022-04-05 14:08:20 -0400

    switch from moment js to luxon

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,53 +1,10 @@
+let DateTime = luxon.DateTime;
 
-// current date
-var dateContainer = $('#date-container')
-var newDate = moment().format('dddd MMMM Do');
-var date = "<p>" + newDate + "</p>";
-var currentDate = $(date);
-
-currentDate.attr('id', 'date');
-currentDate.addClass('justify-center');
-dateContainer.append(date);
-
-// hour text (9:00, 10:00 ...)
-var hourContainer = $('#hour-container')
-var hourDiv = $('#hour-div');
-var hourText = '<div>' + moment().hour() + '</div>';
-$(hourText).attr('hour-text')
-$(hourText).addClass('text-right flex-end items-center')
-hourDiv.append(hourText);
-hourContainer.append(hourDiv);
-
-// task section
-var taskInput = $('task-input');
-var task = $('#task-input');
-
-var taskSection = $('#task-section');
-var taskList = document.createElement('ul');
-
-taskSection.on('click', () => {
-  taskInput.show()
-});
-
-// save section
-let saveBtn = $('#save-button');
-let saveDiv = $('#save-div');
-let saved = false;
-
-saveTask = () => {
-  // add task to task list
-  let aBlock = document.createElement('ul').appendChild(document.createElement('p'));
-};
-
-saveBtn.on('click', () => {
-  if (!task.value) {
-    console.log('needs task')
-
-  }
-  saveTask();
-  console.log('saving task')
-});
-
-// current date desired format
-// current hour desired format
-// moment().format('LT');   // 10:12 AM
+let dateContainer = $('#date-container');
+let today = DateTime.local().toLocaleString(DateTime.DATETIME_MED);
+let currentDateEl = document.createElement('p');
+$(currentDateEl).attr('current-date')
+$(currentDateEl).addClass('text-center text-xl font-black')
+currentDateEl.innerHTML = today;
+dateContainer.append(currentDateEl);
+// currentDateText = today;
