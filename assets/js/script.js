d9dc712749008dc332dce50dcff7a9f1fd616999
Matthew Pryor <matthewapryor@gmail.com>
2022-04-05 11:35:28 -0400

    current hour appears in hour div's hour text

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,54 @@
-let taskDiv = $('#task-div');
-let saveDiv = $('#save-div');
-let saveBtn = $('#save-button');
-let textarea = $('#task-textarea');
-let textareaDiv = $('#task-textarea-div');
-let presentDate = $('#present-date');
-let saved = false;
 
-taskDiv.on('click', () => {
-  textareaDiv.hide();
-  textarea.show().focus();
-  saveTask();
+// current date
+var dateContainer = $('#date-container')
+var newDate = new Date('01/12/2016');
+var date = "<p>" + newDate + "</p>";
+var currentDate = $(date);
+
+currentDate.attr('id', 'date');
+currentDate.addClass('justify-center');
+dateContainer.append(date);
+
+// hour text (9:00, 10:00 ...)
+var hourContainer = $('#hour-container')
+var hourDiv = $('#hour-div');
+var hourText = '<div>' + moment().hour() + '</div>';
+$(hourText).attr('hour-text')
+$(hourText).addClass('text-right flex-end items-center')
+hourDiv.append(hourText);
+hourContainer.append(hourDiv);
+
+// task section
+var taskInput = $('task-input');
+var task = $('#task-input');
+
+var taskSection = $('#task-section');
+var taskList = document.createElement('ul');
+
+taskSection.on('click', () => {
+  taskInput.show()
 });
 
-saveTask = () => {
-  // do nothing if there isn't any value (text) within the textarea
-  // if (!textarea.value) {
-  //   return
-  // }
-  // console.log('saving task')
-  // if there IS some value in the textarea, 
-  // set the textarea value to be a local storage item
+// save section
+let saveBtn = $('#save-button');
+let saveDiv = $('#save-div');
+let saved = false;
 
+saveTask = () => {
+  // add task to task list
+  let aBlock = document.createElement('ul').appendChild(document.createElement('p'));
 };
 
-saveDiv.on('click', saveBtn, console.log('sat'));
-textarea.hide();
+saveBtn.on('click', () => {
+  if (!task.value) {
+    console.log('needs task')
+
+  }
+  saveTask();
+  console.log('saving task')
+});
+
+// current date desired format
+// moment().format('dddd MMMM Do');
+// current hour desired format
+// moment().format('LT');   // 10:12 AM
